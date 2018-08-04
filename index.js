6ae3d20f6e861cf557f0cdb054bcc505f8595099
Alvaro De La Torre <alvarodltp@gmail.com>
2018-08-04 15:46:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+  let message;
+  if (distance > 2000 && distance <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (distance < 400){
+    message = 'This one is on me!';
+  } else {
+    message = 'No can do.'
+  }
+  return message
+}
+
+function ternaryCheckCity(city) {
+  let message;
+  city === 'NYC' ? message = "Ok, sounds good." : message = "No go.";
+  return message
+}
+
+function switchOnCharmFromTip(tip){
+  let message;
+switch(tip) {
+  case 'generous':
+  message = "Thank you so much."
+  break;
+  case 'not as generous':
+  message = 'Thank you.'
+  break;
+  default:
+  message = "Bye."
+}
+return message;
+}
